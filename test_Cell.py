8742fac979ea07a45966771ac07d19541eae5a3e
Masoud-Ghodrati <22876969+Masoud-Ghodrati@users.noreply.github.com>
2018-02-17 18:01:45 +1100

    two more plot added
    1- Averaged accuracy plot in all levels added.
    2- Half screen accuracy added

--- test_Cell.py
+++ test_Cell.py
@@ -1,14 +1,14 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-fig, axs = plt.subplots(nrows=3, ncols=1)
-axs = axs.ravel()
-t= 0
-cmap = plt.get_cmap('jet',64)
-print(np.ndim(cmap))
+
+a = []
 for i in range(3):
-    print(i)
-    axs[t].plot(np.random.random(100), color=cmap(i*80))
-    axs[t].set_title('o')
-    t +=1
-plt.show()
+    a.append(np.random.random((2,5)))
+print(a)
+b = np.reshape(a, (-1,5))
+print(b[:,[2,3]])
+print(b)
+print(f'the shape is: {np.shape(b)} and the length is {np.ndim(b)}')
+print(f'the mean is : {np.mean(b, axis=0)}')
+# print(np.concatenate(a,[[]],axis=0))
